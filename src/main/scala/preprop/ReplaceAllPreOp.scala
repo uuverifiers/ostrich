8fbfc2673b8686be2ca0ca7179697e84e7bfcc11
Matthew Hague <matthew.hague@rhul.ac.uk>
2018-06-01 16:32:49 +0100

    Fix compilation error in ReplaceAllPreOp
    Other compilation errors did not exist: i was compiling from the wrong
    directory...

--- src/main/scala/preprop/ReplaceAllPreOp.scala
+++ src/main/scala/preprop/ReplaceAllPreOp.scala
@@ -10,7 +10,7 @@ object ReplaceAllPreOp {
 
 class ReplaceAllPreOp(val a : Char) extends PreOp {
 
-  def apply(argumentConstraints : Seq[Automaton],
+  def apply(argumentConstraints : Seq[Seq[Automaton]],
             resultConstraint : Automaton) : Iterator[Seq[Automaton]] = {
     val rc = resultConstraint
     for (box <- CaleyGraph[rc.type](rc).getNodes.iterator) yield {
