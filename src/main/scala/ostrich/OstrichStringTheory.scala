23cf4c6a0400b98b1c7ea833dc41bd7eaafc3bd2
Philipp Ruemmer <ph_r@gmx.net>
2019-10-03 09:14:15 +0200

    modification in API

--- src/main/scala/ostrich/OstrichStringTheory.scala
+++ src/main/scala/ostrich/OstrichStringTheory.scala
@@ -1,6 +1,6 @@
 /*
  * This file is part of Ostrich, an SMT solver for strings.
- * Copyright (C) 2018  Matthew Hague, Philipp Ruemmer
+ * Copyright (C) 2018-2019  Matthew Hague, Philipp Ruemmer
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -51,7 +51,7 @@ class OstrichStringTheory(transducers : Seq[(String, Transducer)],
   val alphabetSize = OstrichStringTheory.alphabetSize
   val upperBound = IdealInt(alphabetSize - 1)
   val CharSort   = ModuloArithmetic.ModSort(IdealInt.ZERO, upperBound)
-  val RegexSort  = new Sort.InfUninterpreted("RegLan")
+  val RegexSort  = Sort.createInfUninterpretedSort("RegLan")
 
 } with AbstractStringTheoryWithSort {
 
