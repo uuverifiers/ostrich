a86c9f7a2b302d15ac5f2417e6f6bb5cf182f257
Zhilei Han <hzl17@mails.tsinghua.edu.cn>
2020-10-28 11:51:02 +0800

    add testcases

--- src/test/scala/SMTLIBTests.scala
+++ src/test/scala/SMTLIBTests.scala
@@ -67,6 +67,10 @@ object SMTLIBTests extends Properties("SMTLIBTests") {
     checkFile("tests/regex_cg_5.smt2", "unsat")
   property("regex_cg_ref.smt2") =
     checkFile("tests/regex_cg_ref.smt2", "sat")
+  property("regex_cg_ref2.smt2") =
+    checkFile("tests/regex_cg_ref2.smt2", "sat")
+  property("regex_cg_ref3.smt2") =
+    checkFile("tests/regex_cg_ref3.smt2", "sat")
 
   property("concat-regex.smt2 eager") =
     checkFileOpts("tests/concat-regex.smt2", "sat", "+eager")
