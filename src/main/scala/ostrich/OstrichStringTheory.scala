5bd31cdf44288e50fb449e92e6ab97657979bbee
Philipp Ruemmer <ph_r@gmx.net>
2021-01-19 16:01:06 +0100

    parsing of re.case_insensitive; renamed re.from.ecma to re.from_ecma

--- src/main/scala/ostrich/OstrichStringTheory.scala
+++ src/main/scala/ostrich/OstrichStringTheory.scala
@@ -83,7 +83,9 @@ class OstrichStringTheory(transducers : Seq[(String, Transducer)],
   val str_reverse =
     MonoSortedIFunction("str.reverse", List(SSo), SSo, true, false)
   val re_from_ecma2020 =
-    MonoSortedIFunction("re.from.ecma2020", List(SSo), RSo, true, false)
+    MonoSortedIFunction("re.from_ecma2020", List(SSo), RSo, true, false)
+  val re_case_insensitive =
+    MonoSortedIFunction("re.case_insensitive", List(RSo), RSo, true, false)
 
   // List of user-defined functions on strings that can be extended
   val extraStringFunctions : Seq[(String, IFunction, PreOp,
@@ -109,7 +111,8 @@ class OstrichStringTheory(transducers : Seq[(String, Transducer)],
       yield (name, Left(f))) ++
      (for ((name, p, _) <- transducersWithPreds.iterator)
       yield (name, Right(p))) ++
-     Iterator((re_from_ecma2020.name, Left(re_from_ecma2020)))).toMap
+     Iterator((re_from_ecma2020.name, Left(re_from_ecma2020)),
+              (re_case_insensitive.name, Left(re_case_insensitive)))).toMap
 
   val extraIndexedOps : Map[(String, Int), Either[IFunction, Predicate]] = Map()
 
@@ -124,7 +127,8 @@ class OstrichStringTheory(transducers : Seq[(String, Transducer)],
 
   val functions =
     predefFunctions ++
-    (extraStringFunctions map (_._2)) ++ List(re_from_ecma2020)
+    (extraStringFunctions map (_._2)) ++
+    List(re_from_ecma2020, re_case_insensitive)
 
   val (funPredicates, _, _, functionPredicateMap) =
     Theory.genAxioms(theoryFunctions = functions,
@@ -163,7 +167,8 @@ class OstrichStringTheory(transducers : Seq[(String, Transducer)],
                    str_to_int, int_to_str,
                    re_none, re_eps, re_all, re_allchar, re_charrange,
                    re_++, re_union, re_inter, re_diff, re_*, re_+, re_opt,
-                   re_comp, re_loop, re_from_str, re_from_ecma2020))
+                   re_comp, re_loop, re_from_str, re_from_ecma2020,
+                   re_case_insensitive))
      yield functionPredicateMap(f)) ++
     (for (f <- List(str_len); if flags.useLength != OFlags.LengthOptions.Off)
      yield functionPredicateMap(f)) ++
