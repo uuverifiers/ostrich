79cad99de0ebc9a47d0934d8febea930a5ab63ec
Amanda Stjerna <amanda.stjerna@it.uu.se>
2021-03-19 17:34:35 +0100

    Base version (before upgrade)

--- build.sbt
+++ build.sbt
@@ -5,13 +5,23 @@ lazy val commonSettings = Seq(
   scalaVersion := "2.11.12",
   crossScalaVersions := Seq("2.11.12", "2.12.10"),
   publishTo := Some(Resolver.file("file",  new File( "/home/wv/public_html/maven/" )) ),
-  scalacOptions += "-deprecation",
+  scalacOptions ++= Seq(
+    "-deprecation",
+    //"-Xfatal-warnings",
+    "-unchecked",
+    "-Xlint",
+    "-Xelide-below", "FINE",
+    // "-feature",
+    // "-optimize",
+    "-Ywarn-dead-code",
+    "-Ywarn-unused"
+  ),
   resolvers += ("uuverifiers" at "http://logicrunch.research.it.uu.se/maven/").withAllowInsecureProtocol(true),
   libraryDependencies += "uuverifiers" %% "princess" % "nightly-SNAPSHOT",
   libraryDependencies += "uuverifiers" % "ecma2020-regex-parser" % "0.4-SNAPSHOT",
-  libraryDependencies += "org.sat4j" % "org.sat4j.core" % "2.3.1",
   libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.14.0" % "test",
-  libraryDependencies += "dk.brics.automaton" % "automaton" % "1.11-8"
+  libraryDependencies += "dk.brics.automaton" % "automaton" % "1.11-8",
+  libraryDependencies += "uuverifiers" %% "parikh-theory" % "0.1.0-SNAPSHOT"
 )
 
 lazy val root = (project in file(".")).
--- src/main/scala/ostrich/Automaton.scala
+++ src/main/scala/ostrich/Automaton.scala
@@ -40,12 +40,20 @@ import ap.terfor.conjunctions.{Conjunction, ReduceWithConjunction}
 
 import scala.collection.mutable.{BitSet => MBitSet,
                                  HashMap => MHashMap, HashSet => MHashSet,
-                                 ArrayStack}
+  ArrayStack}
+
+import ap.parser.{IExpression, IFormula}
+import uuverifiers.parikh_theory.{
+  Automaton => OtherAutomaton,
+  LengthCounting
+}
+
 
 /**
  * Interface for different implementations of finite-state automata.
  */
 trait Automaton {
+
   /**
    * Union
    */
@@ -190,7 +198,7 @@ trait TLabelEnumerator[TLabel] {
  * don't have any structure and are not composite, there is a unique
  * initial state, and a set of accepting states.
  */
-trait AtomicStateAutomaton extends Automaton {
+trait AtomicStateAutomaton extends Automaton with OtherAutomaton {
   /**
    * Type of states
    */
@@ -200,6 +208,8 @@ trait AtomicStateAutomaton extends Automaton {
    * Type of labels
    */
   type TLabel
+
+  type Label = TLabel
 
   /**
    * Operations on labels
@@ -257,9 +267,9 @@ trait AtomicStateAutomaton extends Automaton {
   /**
    * Iterate over all transitions
    */
-  def transitions : Iterator[(State, TLabel, State)] =
+  override def transitions : Iterator[(State, TLabel, State)] =
     for (s1 <- states.iterator; (s2, lbl) <- outgoingTransitions(s1))
-      yield (s1, lbl, s2)
+    yield (s1, lbl, s2)
 
   /**
    * Get image of a set of states under a given label
@@ -335,151 +345,14 @@ trait AtomicStateAutomaton extends Automaton {
   }
 
   /**
-   * Compute the length abstraction of this automaton. Special case of
-   * Parikh images, following the procedure in Verma et al, CADE 2005
-   */
-  lazy val getLengthAbstraction : Formula = /* Exploration.measure("length abstraction") */ {
+    * Compute the length abstraction of this automaton.
+    */
+  lazy val getLengthAbstraction: Formula = {
     import TerForConvenience._
     implicit val order = TermOrder.EMPTY
 
-    val stateSeq = states.toIndexedSeq
-    val state2Index = stateSeq.iterator.zipWithIndex.toMap
-
-    lazy val preStates = {
-      val preStates = Array.fill(stateSeq.size)(new MBitSet)
-
-      for ((from, _, to) <- transitions)
-        preStates(state2Index(to)) += state2Index(from)
-
-      preStates
-    }
-
-    lazy val transPreStates = {
-      val transPreStates = Array.fill(stateSeq.size)(new MBitSet)
-
-      for ((s1, s2) <- preStates.iterator zip transPreStates.iterator)
-        s2 ++= s1
-
-      for ((s, n) <- transPreStates.iterator.zipWithIndex)
-        s += n
-
-      // fixed-point iterator, to find transitively referenced states
-      var changed = true
-      while (changed) {
-        changed = false
-
-        for (i <- 0 until transPreStates.size) {
-          val set = transPreStates(i)
-
-          val oldSize = set.size
-          for (j <- 0 until transPreStates.size)
-            if (set contains j)
-              set |= transPreStates(j)
-
-          if (set.size > oldSize)
-            changed = true
-        }
-      }
-
-      transPreStates
-    }
-
-    val initialStateInd = state2Index(initialState)
-
-    ////////////////////////////////////////////////////////////////////////////
-
-    disjFor(for (finalState <- acceptingStates)
-            yield (uniqueLengthStates get finalState) match {
-
-    case Some(len) =>
-      v(0) === len
-
-    case None => {
-      val finalStateInd = state2Index(finalState)
-      val refStates = transPreStates(finalStateInd)
-
-      val productions : List[(Int, Option[Int])] =
-        (if (refStates contains initialStateInd)
-           List((initialStateInd, None))
-         else List()) :::
-        (for (state <- refStates.iterator;
-              preState <- preStates(state).iterator)
-         yield (state, Some(preState))).toList
-
-      val (prodVars, zVars, sizeVar) = {
-        val prodVars = for ((_, num) <- productions.zipWithIndex) yield v(num)
-        var nextVar = prodVars.size
-        val zVars = (for (state <- refStates.iterator) yield {
-          val ind = nextVar
-          nextVar = nextVar + 1
-          state -> v(ind)
-        }).toMap
-        (prodVars, zVars, v(nextVar))
-      }
-
-      // equations relating the production counters
-      val prodEqs =
-        (for (state <- refStates.iterator) yield {
-          LinearCombination(
-             (if (state == finalStateInd)
-                Iterator((IdealInt.ONE, OneTerm))
-              else
-                Iterator.empty) ++
-             (for (((source, targets), prodVar) <-
-                      productions.iterator zip prodVars.iterator;
-                    mult = (if (targets contains state) 1 else 0) -
-                           (if (source == state) 1 else 0))
-              yield (IdealInt(mult), prodVar)),
-             order)
-        }).toList
-
-      val sizeEq =
-        LinearCombination(
-          (for (((_, Some(_)), v) <- productions.iterator zip prodVars.iterator)
-           yield (IdealInt.ONE, v)) ++
-          Iterator((IdealInt.MINUS_ONE, sizeVar)),
-          order)
-
-      val entryZEq =
-        zVars(finalStateInd) - 1
-
-      val allEqs = eqZ(entryZEq :: sizeEq :: prodEqs)
-
-      val prodNonNeg =
-        prodVars >= 0
-
-      val prodImps =
-        (for (((source, _), prodVar) <-
-                productions.iterator zip prodVars.iterator;
-              if source != finalStateInd)
-         yield ((prodVar === 0) | (zVars(source) > 0))).toList
-
-      val zImps =
-        (for (state <- refStates.iterator; if state != finalStateInd) yield {
-           disjFor(Iterator(zVars(state) === 0) ++
-                   (for (((source, targets), prodVar) <-
-                           productions.iterator zip prodVars.iterator;
-                         if targets contains state)
-                    yield conj(zVars(state) === zVars(source) + 1,
-                               geqZ(List(prodVar - 1, zVars(source) - 1)))))
-         }).toList
-
-      val matrix =
-        conj(allEqs :: prodNonNeg :: prodImps ::: zImps)
-      val rawConstraint =
-        exists(prodVars.size + zVars.size, matrix)
-
-      val constraint =
-        ap.util.Timeout.withTimeoutMillis(1000) {
-          // best-effort attempt to get a quantifier-free version of the
-          // length abstraction
-          PresburgerTools.elimQuantifiersWithPreds(rawConstraint)
-        } {
-          ReduceWithConjunction(Conjunction.TRUE, order)(rawConstraint)
-        }
-
-      constraint
-    }})
+    val length = v(0)
+    (LengthCounting(IndexedSeq(this))) allowsMonoidValues Seq(length)
   }
 }
 
