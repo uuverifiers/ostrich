ab7ae955cd8c469f1e6c29af3851d7e823176976
SimpleXiaohu <1048448520@qq.com>
2022-11-17 19:57:50 +0800

    fix bug about wrong extend to TermGenerator.order

---
+++ src/main/scala/ostrich/parikh/OstrichConfig.scala
@@ -0,0 +1,44 @@
+package ostrich.parikh
+import pureconfig._
+import pureconfig.generic.auto._
+
+object OstrichConfig {
+
+  sealed trait ProductStrategy
+  case class Basic() extends ProductStrategy
+  case class Lazy() extends ProductStrategy
+
+  sealed trait Backend
+  case class Baseline() extends Backend
+  case class Unary() extends Backend
+  case class Catra() extends Backend
+  
+  case class ParikhConf(
+      strategy: ProductStrategy = Basic(),
+      backend: Backend = Baseline(),
+      measureTime: Boolean = true,
+      useCostEnriched: Boolean = false
+  )
+
+  lazy val config: ConfigReader.Result[ParikhConf] = ConfigSource.default.load[ParikhConf]
+
+  lazy val productStrategy = config.right.get.strategy
+
+  var backend = config.right.get.backend
+
+  lazy val measureTime = config.right.get.measureTime
+
+  var useCostEnriched = config.right.get.useCostEnriched
+
+  val outputdot = true
+
+  val regex2ce = true
+
+  val log = true 
+
+  productStrategy match {
+    case Basic() => Console.err.println("Eager product")
+    case Lazy() => Console.err.println("Lazy product")
+  }
+
+}
