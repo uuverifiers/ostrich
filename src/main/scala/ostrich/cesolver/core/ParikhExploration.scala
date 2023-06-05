156a9e48fcfb7f3cf873cb3d0a5be54405d29a1e
SimpleXiaohu <1048448520@qq.com>
2023-06-05 09:41:21 +0800

    some optimizations, product eagerly

--- src/main/scala/ostrich/cesolver/core/ParikhExploration.scala
+++ src/main/scala/ostrich/cesolver/core/ParikhExploration.scala
@@ -194,7 +194,7 @@ class ParikhExploration(
   private def trivalConflict: ConflictSet = {
     for (
       t <- leafTerms.toSeq;
-      aut <- constraintStores(t).getContents
+      aut <- constraintStores(t).getCompleteContents
     ) yield TermConstraint(t, aut)
   }
 
