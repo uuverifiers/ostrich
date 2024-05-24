d05c3c757526e9d1d3a1009e579c289af23e5c2a
SimpleXiaohu <1048448520@qq.com>
2024-05-24 15:40:19 +0800

    add copyrights for ceasolver

--- src/main/scala/ostrich/cesolver/stringtheory/CEStringTheoryBuilder.scala
+++ src/main/scala/ostrich/cesolver/stringtheory/CEStringTheoryBuilder.scala
@@ -1,6 +1,6 @@
 /**
  * This file is part of Ostrich, an SMT solver for strings.
- * Copyright (c) 2019-2023 Denghang Hu, Philipp Ruemmer. All rights reserved.
+ * Copyright (c) 2023 Denghang Hu, Matthew Hague, Philipp Ruemmer. All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
