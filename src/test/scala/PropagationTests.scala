9be99d4f08d280bfc9e12db1a080c560389c2944
Oliver Markgraf <markgraf@cs.uni-kl.de>
2024-05-29 11:46:00 +0200

    change timeout

--- src/test/scala/PropagationTests.scala
+++ src/test/scala/PropagationTests.scala
@@ -8,7 +8,7 @@ object PropagationTests extends Properties("PropagationTests") {
 
   import System.lineSeparator
 
-  val timeout = 30000
+  val timeout = 60000
 
   def expectResult[A](expResult : String)(computation : => A) : Boolean = {
     val result = asString {
@@ -44,34 +44,19 @@ object PropagationTests extends Properties("PropagationTests") {
                         false)
     }
 
-  property("noodles-unsat.smt2") =
-    checkFileOpts("tests/propagation-benchmarks/noodles-unsat.smt2", "unknown", "", "-timeout=3000")
-  property("noodles-unsat.smt2") =
-    checkFileOpts("tests/propagation-benchmarks/noodles-unsat.smt2", "unknown", "+forward", "-timeout=3000")
-  property("noodles-unsat.smt2") =
-    checkFileOpts("tests/propagation-benchmarks/noodles-unsat.smt2", "unsat", "+forwardBackward")
-  property("noodles-unsat2.smt2") =
-    checkFileOpts("tests/propagation-benchmarks/noodles-unsat2.smt2", "unsat", "+forwardBackward")
-  property("noodles-unsat3.smt2") =
-    checkFileOpts("tests/propagation-benchmarks/noodles-unsat3.smt2", "unsat", "+forwardBackward")
-  property("noodles-unsat4.smt2") =
-    checkFileOpts("tests/propagation-benchmarks/noodles-unsat4.smt2", "unsat", "+forwardBackward")
-  property("noodles-unsat5.smt2") =
-    checkFileOpts("tests/propagation-benchmarks/noodles-unsat5.smt2", "unsat", "+forwardBackward")/*
-  property("noodles-unsat6.smt2") =
-    checkFileOpts("tests/propagation-benchmarks/noodles-unsat6.smt2", "unsat", "+forwardBackward")
-  property("noodles-unsat7.smt2") =
-    checkFileOpts("tests/propagation-benchmarks/noodles-unsat7.smt2", "unsat", "+forwardBackward")
-  property("noodles-unsat8.smt2") =
-    checkFileOpts("tests/propagation-benchmarks/noodles-unsat8.smt2", "unsat", "+forwardBackward")
-  property("noodles-unsat9.smt2") =
-    checkFileOpts("tests/propagation-benchmarks/noodles-unsat9.smt2", "unsat", "+forwardBackward")
-  property("word-equation-6.smt2") =
-    checkFileOpts("tests/propagation-benchmarks/word-equation-6.smt2", "unknown", "+forwardBackward","-timeout=3000")
-  property("03_track_1.smt2") =
-    checkFileOpts("tests/propagation-benchmarks/03_track_1.smt2", "unsat", "+forwardOnly")
-  property("03_track_10.smt2.smt2") =
-    checkFileOpts("tests/propagation-benchmarks/03_track_10.smt2", "unsat", "+forwardOnly")
-  property("03_track_11.smt2.smt2") =
-    checkFileOpts("tests/propagation-benchmarks/03_track_11.smt2", "unsat", "+forwardOnly")*/
+    property("noodles-unsat.smt2") =
+      checkFileOpts("tests/propagation-benchmarks/noodles-unsat.smt2", "unknown", "", "-timeout=3000")
+    property("noodles-unsat.smt2") =
+      checkFileOpts("tests/propagation-benchmarks/noodles-unsat.smt2", "unknown", "+forward", "-timeout=3000")
+    property("noodles-unsat.smt2") =
+      checkFileOpts("tests/propagation-benchmarks/noodles-unsat.smt2", "unsat", "+forwardBackward")
+    property("noodles-unsat2.smt2") =
+      checkFileOpts("tests/propagation-benchmarks/noodles-unsat2.smt2", "unsat", "+forwardBackward")
+    property("noodles-unsat3.smt2") =
+      checkFileOpts("tests/propagation-benchmarks/noodles-unsat3.smt2", "unsat", "+forwardBackward")
+    property("noodles-unsat4.smt2") =
+      checkFileOpts("tests/propagation-benchmarks/noodles-unsat4.smt2", "unsat", "+forwardBackward")
+    property("noodles-unsat5.smt2") =
+      checkFileOpts("tests/propagation-benchmarks/noodles-unsat5.smt2", "unsat", "+forwardBackward")
+
 }
