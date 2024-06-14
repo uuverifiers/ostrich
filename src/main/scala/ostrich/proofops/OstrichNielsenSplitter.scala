a0c461af931a2465c3b7a1687a36c4696975ee52
Matthew Hague <matthew.hague@rhul.ac.uk>
2024-06-14 17:59:42 +0100

    Remove trailing spaces from OstrichNielsenSplitter

--- src/main/scala/ostrich/proofops/OstrichNielsenSplitter.scala
+++ src/main/scala/ostrich/proofops/OstrichNielsenSplitter.scala
@@ -1,21 +1,21 @@
 /**
  * This file is part of Ostrich, an SMT solver for strings.
  * Copyright (c) 2022 Matthew Hague, Philipp Ruemmer. All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * * Redistributions of source code must retain the above copyright notice, this
  *   list of conditions and the following disclaimer.
- * 
+ *
  * * Redistributions in binary form must reproduce the above copyright notice,
  *   this list of conditions and the following disclaimer in the documentation
  *   and/or other materials provided with the distribution.
- * 
+ *
  * * Neither the name of the authors nor the names of their
  *   contributors may be used to endorse or promote products derived from
  *   this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
@@ -549,7 +549,7 @@ class OstrichNielsenSplitter(goal : Goal,
       ReduceWithConjunction(lengthModel, extOrder)
 
 /*
-    for (t <- 
+    for (t <-
     (for (lit <- concatLits.iterator;
           t <- lit.iterator;
           if !t.isConstant)
