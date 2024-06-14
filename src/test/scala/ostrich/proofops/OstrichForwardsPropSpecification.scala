d2ed1cec4d29bd57ae3fda566f463a541019876d
Matthew Hague <matthew.hague@rhul.ac.uk>
2024-06-14 17:53:02 +0100

    add new forwards prop class
    Goal is to replace the monolithic OstrichSolver with a class for each
    proof rule. This can lead to better integration between e.g. Nielsen
    rewrites and regular constraint propagation.

---
+++ src/test/scala/ostrich/proofops/OstrichForwardsPropSpecification.scala
@@ -0,0 +1,16 @@
+
+package ostrich.proofops
+
+import ostrich.automata.{BricsAutomaton, Automaton, IDState}
+
+import org.scalacheck.Properties
+import org.scalacheck.Prop._
+
+object OstrichForwardsPropSpecification
+  extends Properties("OstrichForwardsProp") {
+
+  property("Simple Test") = {
+    // TODO
+    true
+  }
+}
