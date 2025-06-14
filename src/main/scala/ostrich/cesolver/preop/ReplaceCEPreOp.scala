a3cba3c08de2d350fe6d274f61677abf109bfb08
SimpleXiaohu <1048448520@qq.com>
2025-06-14 10:42:34 +0800

    remove useless debug info

--- src/main/scala/ostrich/cesolver/preop/ReplaceCEPreOp.scala
+++ src/main/scala/ostrich/cesolver/preop/ReplaceCEPreOp.scala
@@ -350,8 +350,6 @@ object ReplaceCEPreOpShortestRegEx {
     )
     val labels = labelEnumerator.enumDisjointLabelsComplete
 
-    println("labels: " + labels)
-
     val ceTran = new CETransducer
     val nop = OutputOp("", NOP, "")
     val copy = OutputOp("", Plus(0), "")
